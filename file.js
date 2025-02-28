a9b92941ec10d79ed3bbdf9228b2e8f222702b56
Milton <107018581+milton-henschel@users.noreply.github.com>
2025-02-28 09:47:23 +0000

    Add files via upload

---
+++ file.js
@@ -0,0 +1,16 @@
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "flex";
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
+// Close modal when clicking outside of it
+window.onclick = function(event) {
+    document.querySelectorAll(".modal").forEach(modal => {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+};
